779a477da99732c3407ec00427e9ac0be16896d3
Antony Male <antony.male@gmail.com>
2015-09-05 19:03:53 +0100

    Add 'SeparateMigrator' example

---
+++ examples/SeparateMigrator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleMigrations.Console;
+using SimpleMigrations.VersionProvider;
+
+namespace SeparateMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SQLiteConnection("DataSource=database.sqlite");
+            var runner = new ConsoleRunner(Assembly.GetExecutingAssembly(), db, new SqliteVersionProvider());
+            runner.Run(args);
+        }
+    }
+}
