465e23aa0c50305d52dc8c5322a2cd153c0895ec
Rik Svendsen Rose <rsr@gait.dk>
2016-10-13 20:48:11 +0200

    Add test for MsSqlVersionProviderTests

---
+++ SimpleMigratorTests/MsSqlVersionProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using SimpleMigrations;
+using SimpleMigrations.Console;
+using SimpleMigrations.VersionProvider;
+
+namespace SimpleMigratorTests
+{
+    [TestFixture]
+    public class MsSqlVersionProviderTests
+    {
+        [Test]
+        public static void Verify_MsSqlMigration()
+        {
+            const string connectionString = @"Server=.\SQLEXPRESS;Database=SimpleMigratorTests;Trusted_Connection=True;";
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString) )
+            {
+                connection.Open();
+                var migrationsAssembly = typeof(MsSqlVersionProviderTests).Assembly;
+                var versionProvider = new MsSqlVersionProvider();
+                var migrator = new SimpleMigrator(migrationsAssembly, connection, versionProvider);
+                migrator.Load();
+
+                migrator.MigrateToLatest();
+
+            }
+        }
+    }
+}
---
+++ SimpleMigratorTests/TestMigrations/AddUser.cs
@@ -0,0 +1,20 @@
+﻿using SimpleMigrations;
+
+namespace SimpleMigratorTests.TestMigrations
+{
+    [Migration(1, useTransaction:false)]
+    public class AddUser : Migration
+    {
+        public override void Up()
+        {
+            Execute(@"CREATE TABLE Users (
+                [Id] [int] IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+                Name TEXT NOT NULL);");
+        }
+
+        public override void Down()
+        {
+            Execute("DROP TABLE Users");
+        }
+    }
+}
