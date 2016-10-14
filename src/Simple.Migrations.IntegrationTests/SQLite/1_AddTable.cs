0b0adbc4a96b4ab2e00a457bfacfd444b15b9105
Antony Male <antony.male@gmail.com>
2016-10-14 13:54:02 +0100

    Start of a project to actually test the different database providers

---
+++ src/Simple.Migrations.IntegrationTests/SQLite/1_AddTable.cs
@@ -0,0 +1,25 @@
+﻿using SimpleMigrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple.Migrations.IntegrationTests.SQLite
+{
+    [Migration(1)]
+    public class AddTable : Migration
+    {
+        public override void Up()
+        {
+            Execute(@"CREATE TABLE Users (
+                Id SERIAL NOT NULL PRIMARY KEY,
+                Name TEXT NOT NULL
+            )");
+        }
+
+        public override void Down()
+        {
+            Execute("DROP TABLE Users");
+        }
+    }
+}
---
+++ src/Simple.Migrations.IntegrationTests/SQLite/SQLiteTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using SimpleMigrations;
+using SimpleMigrations.VersionProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Simple.Migrations.IntegrationTests.SQLite
+{
+    [TestFixture]
+    public class SQLiteTests
+    {
+        private SimpleMigrator migrator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var connection = new SqliteConnection(ConnectionStrings.SQLite);
+            this.migrator = new SimpleMigrator(typeof(SQLiteTests).Assembly, connection, new SQLiteVersionProvider());
+            this.migrator.Load();
+            this.migrator.MigrateTo(0);
+        }
+
+        [Test]
+        public void RunMigration()
+        {
+            this.migrator.MigrateToLatest();
+        }
+    }
+}
